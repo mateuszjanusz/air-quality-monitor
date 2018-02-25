04f9c5f4a2ad6e23a069bb40b9888b027cb55b4b
Mateusz <mateuszjanusz95@gmail.com>
2018-02-25 16:20:07 +0000

    process.env

--- api/db.js
+++ api/db.js
@@ -1,6 +1,12 @@
-const mysql = require('mysql');
+const mysql = require('mysql')
+console.log(process.env)
 const config = require('./config')
 
-const connection = mysql.createPool(config)
+const connection = mysql.createPool({
+	host: process.env.host || config.host,
+	database: process.env.database || config.database,
+	user: process.env.user || config.user,
+	password: process.env.password || config.password,
+})
 
 module.exports = connection
